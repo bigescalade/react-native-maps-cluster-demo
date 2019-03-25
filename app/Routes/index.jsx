1fe242fdf90eff4309733aff95a6ad6e66978ea9
Rob Cunning <rjacunning@gmail.com>
2019-03-25 20:50:26 +1100

    Added and linked icons library

--- app/Routes/index.jsx
+++ app/Routes/index.jsx
@@ -1,20 +1,36 @@
 import React from 'react'
-import { Text } from 'react-native'
+import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons'
 import { createAppContainer, createBottomTabNavigator } from 'react-navigation'
 
 import colors from '../styles/colors'
 import Map from '../views/Map'
 
+const iconSize = 20
+
 const TabNavigator = createBottomTabNavigator(
   {
     Tab1: {
       navigationOptions: {
+        tabBarIcon: ({ focused }) => (
+          <MaterialCommunityIcons
+            color={focused ? colors.primary : colors.gray}
+            name="map-marker-circle"
+            size={iconSize}
+          />
+        ),
         tabBarLabel: 'Map',
       },
       screen: Map,
     },
     Tab2: {
       navigationOptions: {
+        tabBarIcon: ({ focused }) => (
+          <MaterialCommunityIcons
+            color={focused ? colors.primary : colors.gray}
+            name="receipt"
+            size={iconSize}
+          />
+        ),
         tabBarLabel: 'Native View',
       },
       screen: Map,
