382b41762d38bcf8225eff3d4df863df771d40c5
Rob Cunning <rjacunning@gmail.com>
2019-03-26 09:14:24 +1100

    Added clustering lib

---
+++ app/helpers/places.js
@@ -0,0 +1,9 @@
+const places = [
+  {
+    name: 'Vue de monde',
+    lat: -37.818737,
+    long: 144.957565,
+  },
+]
+
+export default places
--- app/views/Map/index.jsx
+++ app/views/Map/index.jsx
@@ -1,7 +1,8 @@
 import React from 'react'
-import { Text } from 'react-native'
-import MapView from 'react-native-maps'
+import MapView from 'react-native-map-clustering'
+import { Marker, PROVIDER_GOOGLE } from 'react-native-maps'
 
+import places from '../../helpers/places'
 import * as styles from './styles'
 
 const Map = () => {
@@ -13,14 +14,20 @@ const Map = () => {
   return (
     <View>
       <MapView
-        initialRegion={{
+        clustering
+        // provider={PROVIDER_GOOGLE}
+        region={{
           latitude: -37.813629,
           longitude: 144.963058,
-          latitudeDelta: 0.03,
-          longitudeDelta: 0.03,
+          latitudeDelta: 0.05,
+          longitudeDelta: 0.05,
         }}
         style={mapStyle}
-      />
+      >
+        {places.map(place => (
+          <Marker coordinate={{ latitude: place.lat, longitude: place.long }} cluster />
+        ))}
+      </MapView>
     </View>
   )
 }
